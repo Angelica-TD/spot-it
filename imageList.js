a291546861ff01466787662839d1a4bd5603c902
Angelica-TD <71614782+Angelica-TD@users.noreply.github.com>
2023-01-19 18:20:03 +1100

    Create imageList.js

---
+++ imageList.js
@@ -0,0 +1,1 @@
+let imageList = ["balloon", "hand", "ok", "ice", "igloo", "lightbulb", "carrot", "lips", "fire", "skull", "art", "maple", "bomb", "beetle", "heart", "snowflake", "apple", "tree", "web", "scissors"];
